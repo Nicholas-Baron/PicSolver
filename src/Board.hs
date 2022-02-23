2b6a4bc49397fbd294f3117ac6f0b48ecbda3b15
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2022-02-23 02:25:41 -0800

    Inital commit

---
+++ src/Board.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Board where
+
+import Data.List (foldl', sort)
+
+data Block
+  = On
+  | Off
+  | Unknown
+  deriving (Show, Eq)
+
+-- A row is a list of blocks and whether they are on, off, or unknown
+
+newtype Row = MkRow [Block]
+  deriving (Show, Eq)
+
+toConstraint :: Row -> RowConstraint
+toConstraint (MkRow row) = go [] row
+  where
+    go :: [Integer] -> [Block] -> RowConstraint
+    -- The constraint is built in reverse order
+    go constraint [] = reverse $ filter (/= 0) constraint
+    -- Starting a constraint
+    go [] (block : blocks) = go [1 | block == On] blocks
+    go constraint@(x : rest) (block : blocks) = case block of
+      On -> go (x + 1 : rest) blocks
+      _ -> go (0 : constraint) blocks
+
+-- A row matches its constraint if:
+--    1. it's constraint is <= the minRowLength of the constraint
+--    2. none of its constraint blocks are longer than the longest in the constraint
+--    3. for each block in the row, the block in the row is <= a block in the same relative order in the constraint
+matchesConstraint :: Row -> RowConstraint -> Bool
+matchesConstraint row constraint =
+  (minRowLength rowConstraint <= minRowLength constraint)
+    && (maximum rowConstraint <= maximum constraint)
+    && relativeOrderEq rowConstraint constraint
+  where
+    rowConstraint = toConstraint row
+
+    relativeOrderEq :: RowConstraint -> RowConstraint -> Bool
+    relativeOrderEq lhs rhs =
+      null $
+        foldl'
+          (\(currentBlock : restRow) block -> if currentBlock <= block then restRow else currentBlock : restRow)
+          lhs
+          rhs
+
+-- A row constraint is a list of run length encoded "on" blocks
+type RowConstraint = [Integer]
+
+minRowLength :: RowConstraint -> Integer
+minRowLength row = sum row + gaps
+  where
+    gaps :: Integer
+    gaps = toInteger $ length row - 1
---
+++ tests/test.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Board
+import Test.Tasty
+import qualified Test.Tasty.HUnit as HU
+import qualified Test.Tasty.QuickCheck as QC
+
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [boardTests]
+
+boardTests :: TestTree
+boardTests = testGroup "Board Tests" [rowTests]
+
+rowTests :: TestTree
+rowTests =
+  testGroup
+    "Row Tests"
+    [ HU.testCase "toConstraint" $
+        HU.assertEqual
+          "toConstraint ignores the number of off or unknown blocks"
+          (toConstraint $ MkRow [On, Off, On, On, On, Off, On])
+          (toConstraint $ MkRow [On, Off, Unknown, On, On, On, Off, Off, On]),
+      QC.testProperty
+        "at least 1 On"
+        (\(MkRow contents) -> On `elem` contents),
+      QC.testProperty
+        "matchesConstraint"
+        (\row@(MkRow contents) -> matchesConstraint row (toConstraint row)),
+      QC.testProperty
+        "minRowLength"
+        (\row@(MkRow contents) -> minRowLength (toConstraint row) <= toInteger (length contents))
+    ]
+
+instance QC.Arbitrary Row where
+  arbitrary :: QC.Gen Row
+  arbitrary = MkRow <$> QC.suchThat arbitraryRow (elem On)
+    where
+      arbitraryRow :: QC.Gen [Block]
+      arbitraryRow = QC.listOf1 arbitraryBlock
+
+      arbitraryBlock :: QC.Gen Block
+      arbitraryBlock = QC.elements [On, Off, Unknown]
