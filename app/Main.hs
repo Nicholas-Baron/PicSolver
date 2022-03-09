10f575d272d29d1b8652d144352e4871dd104ee3
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2022-03-09 14:38:01 -0800

    Read in board setup

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,25 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 import BoardKnowledge
 import Row (RowConstraint)
+import System.Environment (getArgs)
+import System.Exit (die)
+import Text.JSON
 import Util
 
 main :: IO ()
 main = do
+  args <- getArgs
+
+  input_file <- if null args then die "Input file not specified" else return (head args)
+
+  input_text <- readFile input_file
+
+  Constraints {boardSize, columnConstraints, rowConstraints} <- case decodeStrict input_text of Ok x -> return x; Error msg -> die msg
+
   let knownMatrix = fromPossibles possibleBoards
-      possibleBoards = fromConstraints exampleBoardSize rowConstraints columnConstraints
+      possibleBoards = fromConstraints boardSize rowConstraints columnConstraints
       boardImprovements = iterateWhileDiff (improveBoardKnowledge possibleBoards) knownMatrix
 
   let possibleRowCounts = product $ map length $ expandedRows possibleBoards
@@ -27,9 +41,23 @@ main = do
     )
     boardImprovements
 
-exampleBoardSize :: Int
-exampleBoardSize = 10
+data Constraints = Constraints
+  { boardSize :: Int,
+    columnConstraints :: [RowConstraint],
+    rowConstraints :: [RowConstraint]
+  }
+  deriving (Show)
 
-columnConstraints, rowConstraints :: [RowConstraint]
-rowConstraints = [[5], [2, 2, 1], [2, 3, 1], [4, 3, 1], [1, 1, 1, 1], [3, 1], [5], [1, 2, 1], [1, 1, 1], [7, 1]]
-columnConstraints = [[2], [4, 1], [2, 2, 4], [1, 2, 1, 1], [1, 1, 1, 2, 1], [4, 5], [1, 1, 2, 1], [3, 1], [], [5, 3]]
+instance JSON Constraints where
+  readJSON (input :: JSValue) = do
+    object <- case input of JSObject object -> Ok (fromJSObject object); _ -> Error (show input ++ " is not a JSObject")
+
+    let fallibleLookup name = case lookup name object of
+          Nothing -> Error ("Could not find " ++ name ++ " in " ++ show object)
+          Just x -> readJSON x
+
+    boardSize <- fallibleLookup "boardSize"
+    rowConstraints <- fallibleLookup "rowConstraints"
+    columnConstraints <- fallibleLookup "columnConstraints"
+
+    return $ Constraints {boardSize, columnConstraints, rowConstraints}
